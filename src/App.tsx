719074f0686b48fa6ddeff5367df868afed0aea0
AnkitTukatek <ankit.kumar@tukatek.com>
2025-06-05 13:01:57 +0530

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import {Button} from "../../../shared-ui/src/components/Button"
+import { Button } from "../../../shared-ui/src/components/Button";
+import image3 from "./assets/app3.svg";
+
 function App() {
   return (
     <div>
       <h1>Hello from App Three</h1>
       <Button />
+      <img src={image3} alt="App Illustration" />
     </div>
   );
 }
